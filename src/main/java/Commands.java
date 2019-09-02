1a1e1e8c2d0f34a1dc1b94e65416734452353826
Cameron <camobiwon@yahoo.com>
2019-09-02 01:12:41 -0700

    Text changes / contains to go with commands

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -32,6 +32,12 @@ public class Commands implements MessageCreateListener {
             );
         }
 
+        //Contains
+        if (event.getMessageContent().toLowerCase().contains("bone work")) {
+            event.getChannel().sendMessage("bone work");
+            Main.logSend();
+        }
+
         //Custom commands
         switch (event.getMessageContent().toLowerCase()) {
             case "!shutdown":
@@ -74,7 +80,7 @@ public class Commands implements MessageCreateListener {
                 event.getChannel().sendMessage("<:NotTechDemo:610603749823741963>");
                 Main.logSend();
                 break;
-            case "bone work bad":
+            case "bonework bad":
                 event.getChannel().sendMessage("What the fuck did you just say about the greatest VR game of this century, you little crablet? I'll have you know I lead the MythOS PVP leaderboard, I've been involved in numerous Voidway duplication schemes, and I have over 300 confirmed nullbodies taken down. I am trained in VR physics warfare and I'm the top virtual grip sniper in the entirety of BONEWORKS. You are nothing to me but another crablet. I will wipe you the fuck out with the precision the likes of which has never been seen before on this dimensional plane, mark my fucking words. You think you can get away with saying that shit to us over the BONEWORKS Fan Discord? Think again, fucker. As we speak I am contacting the MythOS Police, and your positional coordinates are being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little you call your opinion. You're fucking dead, kid. I can relocate anywhere, anytime, and I can kill you with over seven hundred different physics objects, and that just in the first stage. Not only am I extensively trained in VR fists combat, but I have access to the entire arsenal of MythOS and I will use it to its full extend to unleash a fucking cryptid to wipe your miserable opinion off the face of this reality, you little shit. If only you could have know what justified anger your little 'clever' opinion was about to bring down upon you, maybe you would have held your fucking wireframe tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn melon-eater. I will sever your connection to the real world and you'll remain a VR Junkie forever. You're fucking dead, kiddo.");
                 Main.logSend();
                 break;
@@ -95,7 +101,7 @@ public class Commands implements MessageCreateListener {
                             .setTitle("BONEWORKS Info")
                             .setDescription("Info about the game")
                             //.setAuthor("Author Name", "http://google.com/", "https://cdn.discordapp.com/embed/avatars/0.png")
-                            .addField("Info Source", "Info being pulled from Steam APIs")
+                            .addField("Info Source", "Info being pulled from Steam game data")
                             .addInlineField("Release Date", JSONReader.getRelease())
                             .addInlineField("Game Price", JSONReader.getGamePrice())
                             .setColor(Color.BLUE);
