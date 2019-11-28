9bd1c0179eddec50791b87b75fa55d3596a3526d
Camobiwon <camobiwon@yahoo.com>
2019-11-27 23:31:29 -0800

    Alyx chat contain

--- src/main/java/org/camobiwon/boneworksbot/ChatContains.java
+++ src/main/java/org/camobiwon/boneworksbot/ChatContains.java
@@ -40,5 +40,14 @@ public class ChatContains implements MessageCreateListener {
             }
             ChatLog.log(event, "Reaction Added");
         }
+
+        if (event.getMessageContent().toLowerCase().contains("alyx")) {
+            try {
+                event.getChannel().sendMessage("While I understand and respect your opinion on Half-Life: Alex, I must say that Boneworks is far superior. The latter incorporates cutting edge technology in to the virtual reality space and is much more focused on gameplay and creating believable interactions in a consistent world, HL:A is more so for those who have been waiting more of the story of Half-Life. The main showcase of this is floating hands being in HL:A and Boneworks having a full body. It's a bold move to help push VR further and innovate in the space, paving the way for future games to increase immersion. HL:A is Valve's main way to sell the Index, and yes, it may have a much more rich and grounded story than Boneworks but on a technical level the chance that the raw interactions will even come close is very slim. This is the Boneworks Discord, what the hell did you think you would get as a response?").get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            ChatLog.log(event, "Message Sent");
+        }
     }
 }
