7efe23bf852260a7b28d19403ff6dc8d8b0ee093
Camobiwon <camobiwon@yahoo.com>
2019-12-31 22:26:18 -0800

    Voting fix

--- src/main/java/org/camobiwon/boneworksbot/Voting.java
+++ src/main/java/org/camobiwon/boneworksbot/Voting.java
@@ -14,14 +14,12 @@ public class Voting implements MessageCreateListener {
         if (event.getMessageAuthor().isBotUser()) return;
 
         if (event.getChannel().getId() == Configuration.voteID || event.getChannel().getId() == 661042292588544000L) {
-            System.out.println("msg sent");
             try {
                 event.getMessage().addReaction("\uD83D\uDD3C").get();
                 event.getMessage().addReaction("\uD83D\uDD3D").get();
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
-            ChatLog.log(event, "Reaction Added");
         }
     }
 }
